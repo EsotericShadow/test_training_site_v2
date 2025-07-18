409992f9f415a7c8ef9ff520887c910b0b7ea116
Gabriel Lacroix <gabriel.lacroix94@icloud.com>
2025-07-18 14:54:39 -0700

    page speed optimization

--- src/app/components/home/hero-section.tsx
+++ src/app/components/home/hero-section.tsx
@@ -3,7 +3,7 @@
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
+
 import HeroImage from './HeroImage';
 import { useGsap } from '@/app/hooks/useGsap';
 import { gsap } from 'gsap';
