e059fb26e586bb35e430cfc97d381745e0519250
Gabriel Lacroix <gabriel.lacroix94@icloud.com>
2025-08-02 19:18:02 -0700

    add evergreen link

--- src/app/components/layout/footer.tsx
+++ src/app/components/layout/footer.tsx
@@ -61,7 +61,11 @@ export default function Footer({ footerContent, popularCourses }: FooterProps) {
           </div>
         </div>
         <div className="mt-16 pt-8 border-t border-gray-800 text-center text-gray-300">
-          <p>{footerContent?.copyright_text || `© ${new Date().getFullYear()} ${footerContent?.company_name}. All Rights Reserved.`}</p>
+          <p>
+            <a href="https://www.evergreenwebsolutions.ca" className="hover:text-yellow-400">
+              {footerContent?.copyright_text || `© ${new Date().getFullYear()} ${footerContent?.company_name}. All Rights Reserved.`}
+            </a>
+          </p>
         </div>
       </div>
     </footer>
